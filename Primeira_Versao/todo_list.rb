1e344f1ec2c83ecdb007fa006e8b8ced56aeafe6
Guilherme Rocha <jguilherme.1997@gmail.com>
2024-12-26 21:52:53 -0300

    implementado método para adicionar as tasks

--- Primeira_Versao/todo_list.rb
+++ Primeira_Versao/todo_list.rb
@@ -1,9 +1,13 @@
 class ToDoList
+  def initialize
+    @task = []
+  end
+
   def start
     exibe_menu
     escolha = gets.chomp.to_i
     case escolha
-      when 1 then 
+      when 1 then adiciona_task
       when 2 then
       when 3 then
       when 4 then
@@ -22,6 +26,15 @@ class ToDoList
     puts "4. Remover Tarefa"
     puts "5. Sair"
     puts "Escolha uma opção: "
+  end
+
+  def adiciona_task
+    puts "Digite o título da tarefa:"
+    titulo = gets.chomp
+    puts "Digite a descrição da tarefa:"
+    descricao = gets.chomp
+    @task << { title: titulo, description: descricao, completed: false }
+    puts "Tarefa adicionada com sucesso"
   end
 end
 

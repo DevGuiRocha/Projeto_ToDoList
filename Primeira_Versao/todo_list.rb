4cf506c2671f330508d41552a7b20361a6f071dd
Guilherme Rocha <jguilherme.1997@gmail.com>
2024-12-26 21:47:20 -0300

    adição do menu, com opções prévias de um CRUD

--- Primeira_Versao/todo_list.rb
+++ Primeira_Versao/todo_list.rb
@@ -1,6 +1,27 @@
 class ToDoList
   def start
+    exibe_menu
+    escolha = gets.chomp.to_i
+    case escolha
+      when 1 then 
+      when 2 then
+      when 3 then
+      when 4 then
+      when 5 
+      else
+    end
+  end
+
+  private
+
+  def exibe_menu
     puts "\n==== ToDo List ===="
+    puts "1. Adiciona Tarefa"
+    puts "2. Listar Tarefas"
+    puts "3. Marcar Tarefa como Concluída"
+    puts "4. Remover Tarefa"
+    puts "5. Sair"
+    puts "Escolha uma opção: "
   end
 end
 

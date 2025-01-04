dfcfd7d2454cdee1629c5c9b1f12498a82afc644
Guilherme Rocha <jguilherme.1997@gmail.com>
2025-01-04 16:31:40 -0300

    Criação do objeto trask, com os métodos que serão utilizados de uma forma geral

--- Segunda_Versao/task.rb
+++ Segunda_Versao/task.rb
@@ -1,2 +1,18 @@
 class Task
+  attr_acessor :title, :description, :completed
+
+  def initialize
+    @title = title
+    @description = description
+    @completed = false
+  end
+
+  def task_completa
+    @completed = true
+  end
+
+  def to_s
+    status = completed ? '[X]' : '[ ]'
+    "#{status}"
+  end
 end

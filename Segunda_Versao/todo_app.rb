8ee660430393d3357349f69ece916ef92a2a6733
Guilherme Rocha <jguilherme.1997@gmail.com>
2025-01-04 16:18:13 -0300

    Criação de um menu básico

--- Segunda_Versao/todo_app.rb
+++ Segunda_Versao/todo_app.rb
@@ -1,2 +1,19 @@
 class ToDoApp
+  def start
+    show_menu
+  end
+
+  private
+
+  def show_menu
+    puts "\n==== ToDo List ===="
+    puts "1. Adicionar Tarefa"
+    puts "2. Listar Tarefas"
+    puts "3. Marcar Tarefa como Concluída"
+    puts "4. Remover Tarefa"
+    puts "5. Sair"
+    print "Escolha uma opção: "
+  end
 end
+
+ToDoApp.new.start

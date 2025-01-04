8932d8f68a2e3109fe223b651a1b4abdc2b095fb
Guilherme Rocha <jguilherme.1997@gmail.com>
2025-01-04 16:25:10 -0300

    criado as defs básicas para a execução da aplicação

--- Segunda_Versao/todo_app.rb
+++ Segunda_Versao/todo_app.rb
@@ -21,16 +21,28 @@ class ToDoApp
 
   def verifica_escolha(escolha)
     case escolha
-      when 1 then
-      when 2 then
-      when 3 then
-      when 4 then
+      when 1 then adiciona_task
+      when 2 then lista_task
+      when 3 then marca_task_completa
+      when 4 then remove_task
       when 5 then
         puts "Programa sendo encerrado. Até Breve!!"
         exit
       else
         puts "Opção incorreta, tente novamente"
     end
+  end
+
+  def adiciona_task
+  end
+
+  def lista_task
+  end
+
+  def marca_task_completa
+  end
+
+  def remove_task
   end
 end
 
